1ece0017bad91dc89e25175b2a46ae1eda8e2edd
Gurnaud <ishemagurnaud0@gmail.com>
2026-03-28 15:30:56 +0100

    [refactor]:Social media app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+
+import { Routes,Route} from 'react-router-dom';
+
+{/*Component imports */}
+import SignUpForm from './auth/forms/SignUpForm';
+import SignInForm from './auth/forms/SignInForm';
+import AuthLayout from './auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import {Home} from './_root/pages';
+
+
+const App = () => {
+  return (
+    <>
+    <main className='flex h-screen'>
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout/>}>
+          <Route path='/sign-up' element={<SignUpForm/>}/>
+          <Route path='/sign-in' element={<SignInForm/>}/>
+        </Route>
+        {/*private routes*/}
+        <Route element={<RootLayout/>}>
+          <Route path='/' element={<Home/>}/>
+        </Route>
+        
+
+      </Routes>
+    </main>
+      
+    </>
+  )
+}
+
+export default App
