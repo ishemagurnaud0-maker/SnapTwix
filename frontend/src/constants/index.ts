0229360695de1a6a510faaa43a99f8e4cdf43d9f
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-03 10:19:56 +0100

    [refactor]:dynamic links on sidebar + bottombar

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,45 @@
+ export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgURL: '/assets/icons/home.svg'
+    },
+    {
+        label: 'Explore',
+        route: '/explore',
+        imgURL: '/assets/icons/wallpaper.svg'
+    },
+    {
+        label: 'Create',
+        route: '/create-post',
+        imgURL: '/assets/icons/add.svg'
+    },
+    {
+        label: 'Profile',
+        route: '/profile',
+        imgURL: '/assets/icons/profile.svg'
+    },
+    {
+        label: 'Saved',
+        route: '/saved',
+        imgURL: '/assets/icons/bookmark.svg'
+    }
+];
+
+export const bottomBarLinks = [
+    {
+        imgUrl: '/assets/icons/home.svg',
+        route: '/',
+        label: 'Home'
+    },
+    {
+        imgUrl:'asserss/icons/wallpaper.svg',
+        route:'/explore',
+        label:'Explore'
+    },
+    {
+        imgUrl:'assets/icons/bookmark.svg',
+        route:'/saved',
+        label:'Saved'
+    }
+]
