75d74376ede0157cd58be2a3b96178606252b054
Gurnaud <ishemagurnaud0@gmail.com>
2026-04-13 21:28:00 +0100

    [refactor]:update-post field validation

--- frontend/src/lib/validations/index.ts
+++ frontend/src/lib/validations/index.ts
@@ -33,3 +33,11 @@ export const checkPasswordValidation = z.object({
   .string()
   .email("Invalid email address.")
 })
+
+export const ProfileValidation = z.object({
+  file: z.custom<File[]>(),
+  name: z.string().min(2, "Name must be at least 2 characters."),
+  username: z.string().min(3, "Username must be at least 3 characters."),
+  email: z.string().email("Invalid email address."),
+
+});
